f5d9e5c44a2ae969793f5efbdd1bb936e6719a0b
Raj <raj073019@gmail.com>
2020-11-10 07:47:03 -0500

    Model forms, relative url and url template inheritance

--- orad/frontpage/views.py
+++ orad/frontpage/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.urls import path
 from .form import Newfile
+from .models import Details
 
 
 # Create your views here.
 
 def front(request):
-    return render(request, 'frontpage/index.html')
+    forms = Details.objects.all()
+    return render(request, 'frontpage/index.html', {'forms': forms})
+
 
 def page(request):
     form = Newfile()
@@ -14,6 +16,16 @@ def page(request):
     if request.method == "POST":
         form = Newfile(request.POST)
         if form.is_valid():
-            form.save(commit = True)
-            return front(request)
-    return render(request, 'frontpage/frontpage.html', {'form':form})
+            form.save(commit=True)
+    return render(request, 'frontpage/frontpage.html', {'form': form})
+
+
+def relative(request):
+    abc = 'Hello'
+    abcd = 100
+    return render(request, 'frontpage/relative.html', {'abc':abc, 'abcd':abcd})
+
+
+def other(request):
+    return render(request, 'frontpage/other.html')
+
--- orad/orad/urls.py
+++ orad/orad/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from frontpage import  views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', views.page, name = 'anything'),
-    path('', views.front, name = 'random')
+    # path('', views.front, name = 'random')
+    path('', include('frontpage.urls'))
 ]
